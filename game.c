deb36590acc128fe5594d4471250bb8d8ee5819f
igorifaresi <igor.ifaresi@gmail.com>
2020-03-01 09:11:44 -0300

    Add player prototype

--- game.c
+++ game.c
@@ -18,6 +18,7 @@ typedef struct Camera {
 enum ComponentTypes {
     BLANK_COMP,
     BLOCK_COMP,
+    PLAYER_COMP,
 };
 
 typedef enum ComponentTypes ComponentType;
@@ -33,11 +34,30 @@ typedef struct Component {
     UpdateFunction update;
 }Component;
 
+enum InputTypes {
+    NOT_INPUT,
+    UP_INPUT,
+    DOWN_INPUT,
+    LEFT_INPUT,
+    RIGHT_INPUT,
+};
+
+typedef enum InputTypes InputType;
+
 Camera main_camera;
 SDL_Texture* background[1024][1024];
 Component grid[1024][1024];
 int h_screen_resolution;
 int v_screen_resolution;
+
+unsigned player_grid_pos_x;
+unsigned player_grid_pos_y;
+struct {
+    unsigned char life;
+    unsigned char power;
+    unsigned char element;
+}player_data;
+
 
 SDL_Texture* sprite_assets[3];
 
@@ -91,20 +111,23 @@ void updateGame(SDL_Renderer* renderer, Camera* camera, char season, size_t actu
     int init_render_block_pos =    (h_screen_resolution / 2) 
                                  - ((camera->v_block_qnt * block_size) / 2)
                                  - ((h_block_qnt * block_size) / 2);
-    int block_offset = (h_block_qnt - camera->v_block_qnt) / 2;
+    int block_offset = h_block_qnt / 2; //wrong calculus
 
-    printf("block_size            -> %d\n", block_size);
-    printf("h_block_qnt           -> %d\n", h_block_qnt);
-    printf("init_render_block_pos -> %d\n", init_render_block_pos);
-    printf("block_offset          -> %d\n", block_offset);
+
+    //printf("block_size            -> %d\n", block_size);
+    //printf("h_block_qnt           -> %d\n", h_block_qnt);
+    //printf("init_render_block_pos -> %d\n", init_render_block_pos);
+    //printf("block_offset          -> %d\n", block_offset);
+
 
     assert(SDL_RenderClear(renderer) != -1);
     for (int y = 0; y < camera->v_block_qnt; y++) {
         int render_pos_x = init_render_block_pos;
         int render_pos_y = block_size * y;
         for (int x = 0; x < h_block_qnt + camera->v_block_qnt; x++) {
-            int grid_pos_x = camera->x + x + block_offset;
+            int grid_pos_x = camera->x + x - block_offset;
             int grid_pos_y = camera->y - y;
+            //printf("[log] grid iterator at %d, %d\n", grid_pos_x, grid_pos_y);
             if ((grid_pos_x >= 0 && grid_pos_x < 1024)
              && (grid_pos_y >= 0 && grid_pos_y < 1024)) {
                 SDL_Texture* back = background[grid_pos_y][grid_pos_x];
@@ -116,13 +139,14 @@ void updateGame(SDL_Renderer* renderer, Camera* camera, char season, size_t actu
                 }
                 Component* it = &grid[grid_pos_y][grid_pos_x];
                 if (it->type != BLANK_COMP) {
-                    printf("rendering comp\n");
+                    printf("[log] rendering comp\n");
                     renderCell(renderer, it->sprite, render_pos_y
                                                    , render_pos_x
                                                    , block_size);
-                    if (it->type != BLOCK_COMP && update_entities
-                    &&  it->last_update_tick != actual_tick) {
+                    if (it->type != BLOCK_COMP && it->type != PLAYER_COMP
+                    && update_entities && it->last_update_tick != actual_tick) {
                         it->update(grid_pos_x, grid_pos_y, season);
+                        updateGame(renderer, camera, season, actual_tick, false);
                         it->last_update_tick = actual_tick;
                     }
                 }
@@ -135,6 +159,46 @@ void updateGame(SDL_Renderer* renderer, Camera* camera, char season, size_t actu
 
 //-----------------------------------------------------------------------------
 
+bool updatePlayer(unsigned* pos_x, unsigned* pos_y, char season, InputType input)
+{
+    bool acted = false;
+    unsigned next_x = *pos_x;
+    unsigned next_y = *pos_y;
+    switch (input)
+    {
+    case UP_INPUT:
+        next_y++;
+        break;
+    case DOWN_INPUT:
+        next_y--;
+        break;
+    case LEFT_INPUT:
+        next_x--;
+        break;
+    case RIGHT_INPUT:
+        next_x++;
+        break;
+    default:
+        break;
+    }
+    if ((next_x >= 0 && next_x < 1024)
+    &&  (next_y >= 0 && next_y < 1024)) {
+        if (grid[next_y][next_x].type == BLANK_COMP) {
+            printf("[log] moving to %d, %d\n", next_x, next_y);
+            grid[next_y][next_x] = grid[*pos_y][*pos_x];
+            grid[*pos_y][*pos_x].type = BLANK_COMP;
+            *pos_y = next_y;
+            *pos_x = next_x;
+            acted = true;
+        } else {
+            printf("[log] cell %d, %d is not blank\n", next_x, next_y);
+        }
+    } else {
+        printf("[log] cell %d, %d is not valid\n", next_x, next_y);
+    }
+    return acted;
+}
+
 //-----------------------------------------------------------------------------
 
 int main()
@@ -144,8 +208,8 @@ int main()
     v_screen_resolution = 600;
 
     assert(SDL_Init(SDL_INIT_VIDEO) >= 0);
-    SDL_Window* win = SDL_CreateWindow("game", 0, 0, 800
-                                                   , 600
+    SDL_Window* win = SDL_CreateWindow("game", 0, 0, h_screen_resolution
+                                                   , v_screen_resolution
                                                    , 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
@@ -154,33 +218,55 @@ int main()
 
     main_camera.x = 0;
     main_camera.y = 0;
-    main_camera.v_block_qnt = 12;
+    main_camera.v_block_qnt = 8;
+    player_grid_pos_x = 0;
+    player_grid_pos_y = 0;
 
     fillBackground(sprite_assets[0]);
     eraseGrid();
     Component c;
-    c.type = BLOCK_COMP;
+    c.type = PLAYER_COMP;
     c.last_update_tick = 0;
     c.sprite = sprite_assets[1];
     c.update = NULL;
-    grid[0][3] = c;
+    grid[0][0] = c;
     for (;;) {
+        InputType input = NOT_INPUT;
         SDL_Event e;
 		if ( SDL_PollEvent(&e) ) {
 			if (e.type == SDL_QUIT)
 				break;
 			else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE)
 				break;
-            else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_i)
+            else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_i) {
                 main_camera.y++;
-            else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_k)
+                updateGame(renderer, &main_camera, 0, 0, false);
+            } else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_k) {
                 main_camera.y--;
-            else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_l)
+                updateGame(renderer, &main_camera, 0, 0, false);
+            } else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_l) {
                 main_camera.x++;
-            else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_j)
+                updateGame(renderer, &main_camera, 0, 0, false);
+            } else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_j) {
                 main_camera.x--;
+                updateGame(renderer, &main_camera, 0, 0, false);
+            }
+
+            if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_w)
+                input = UP_INPUT;
+            else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_s)
+                input = DOWN_INPUT;
+            else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_a)
+                input = LEFT_INPUT;
+            else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_d)
+                input = RIGHT_INPUT;
 		}
-        updateGame(renderer, &main_camera, 0, 0, false);
+        if (input != NOT_INPUT) {
+            if (updatePlayer(&player_grid_pos_x, &player_grid_pos_y, 0, input)) {
+                updateGame(renderer, &main_camera, 0, 0, false);
+                updateGame(renderer, &main_camera, 0, 0, true);
+            }
+        }
     }
     closeGame(win, renderer, sprite_assets, 3);
 
